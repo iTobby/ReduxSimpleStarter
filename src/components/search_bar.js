3fc47d641b81e7f244100bf01af452225ab5c341
Tobby Olusanya <Tobby@iTobby-Mac.local>
2016-04-28 16:36:56 +0200

    React first folder

---
+++ src/components/search_bar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { searchterm: ''};
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.searchterm}
+          onChange={e => this.setState({searchterm: e.target.value })} />
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyDbXVHNmEHXve9xEiHKo8XKeQQk1TEStmI';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//Create a new component
+//To produce some HTML
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+
+//Add the HTML Generated Component to the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
